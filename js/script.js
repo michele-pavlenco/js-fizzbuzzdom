d1da37f86be22256dcf0ccdc3ad5a9013abc5ee7
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-03-28 18:22:05 +0200

    added-css

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 const app = document.getElementById("app");
 let maxNum = 100;
-
-
+let i = 1
 for (let i = 1; i <= maxNum; i++) {
-    
-    if ((i % 3 === 0) && (i % 5 === 0 )){
-        const numOf3n5 = document.createElement ("div");
-         numOf3n5.innerHTML = "FizzBuzz" + i;
-         app.append(numOf3n5);
-         console.log(numOf3n5);
-        }
-    
-    else if (i % 5 === 0){
-        const numOf5 = document.createElement ("div");
-        numOf5.innerHTML = "Buzz" + i ;
-        app.append(numOf5);
-        console.log(numOf5);
-    }
-    
-    else if (i % 3 === 0) {
-        const numOf3 = document.createElement("div");
-        numOf3.innerHTML = "Fizz" + i ;
-        app.append(numOf3);
-        console.log(numOf3);
-      } 
-      else 
-      app.append(i)
-      console.log(i);
-}
+  if (i % 3 === 0 && i % 5 === 0) {
+    const numOf3n5 = document.createElement("div");
+    numOf3n5.setAttribute("class", "n-of-3n5")
+    numOf3n5.innerHTML = "FizzBuzz"  ;
+    app.append(numOf3n5);
+    console.log(numOf3n5);
+  } else if (i % 5 === 0) {
+    const numOf5 = document.createElement("div");
+    numOf5.setAttribute("class", "n-of-3")
+    numOf5.innerHTML = "Buzz"  ;
+    app.append(numOf5);
+    console.log(numOf5);
 
-    
+  } else if (i % 3 === 0) {
+    const numOf3 = document.createElement("div");
+    numOf3.setAttribute("class", "n-of-5")
+    numOf3.innerHTML = "Fizz" ;
+    app.append(numOf3);
+    console.log(numOf3);
+  } else {
+     document.createElement ("div")
+    i.innerHTML =  i
+    app.append(i);
+    console.log(i);
+  }
+}
